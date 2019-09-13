d3ffb0067c8edd05b2ee1e88476f727cdc8c020b
manhhung99 <52267054+manhhung99@users.noreply.github.com>
2019-09-13 10:54:10 +0700

    Update lecture01.py

--- lecture01.py
+++ lecture01.py
@@ -2,3 +2,12 @@ a=4
 b=3
 c=a+b
 print("{0} + {1} = {2}".format(a,b,c))
+print("The value of PI is approximately {0:.3f}".format(3.141592653897823121))
+# read 2 integer number on diffent 2 line
+a=int(input())
+b=int(input())
+# read 2 integer number on 1 line
+c,d=map(int ,input().split())
+# read 2 float number on 1 line
+e,f=map(float ,input().split())
+
